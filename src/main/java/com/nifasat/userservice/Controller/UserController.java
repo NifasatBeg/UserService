c8f1451efaa6df6e6d4ff8a49073fbb7ff4a2f9e
Nifasatb <nifasat.beg@turtlemint.com>
2025-03-22 11:38:44 +0530

    Refactore the code a bit

--- src/main/java/com/nifasat/userservice/Controller/UserController.java
+++ src/main/java/com/nifasat/userservice/Controller/UserController.java
@@ -3,6 +3,7 @@ package com.nifasat.userservice.Controller;
 import com.nifasat.userservice.Entities.UserInfo;
 import com.nifasat.userservice.Entities.UserInfoDto;
 import com.nifasat.userservice.Services.UserService;
+import org.antlr.v4.runtime.misc.NotNull;
 import org.apache.catalina.User;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -31,10 +33,10 @@ public class UserController {
     }
 
     @GetMapping("/user/v1/getUser")
-    public ResponseEntity<UserInfoDto> getUser(UserInfoDto userInfoDto){
+    public ResponseEntity<UserInfoDto> getUser(@RequestParam("user_id") @NotNull String userId){
         try {
-            UserInfoDto user = userService.getUser(userInfoDto);
-            return new ResponseEntity<>(HttpStatus.OK);
+            UserInfoDto user = userService.getUser(userId);
+            return ResponseEntity.ok(user);
         } catch (Exception ex) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
--- src/main/java/com/nifasat/userservice/Services/UserService.java
+++ src/main/java/com/nifasat/userservice/Services/UserService.java
@@ -3,6 +3,8 @@ package com.nifasat.userservice.Services;
 import com.nifasat.userservice.Entities.UserInfo;
 import com.nifasat.userservice.Entities.UserInfoDto;
 import com.nifasat.userservice.Repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.juli.logging.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,7 @@ import java.util.function.Function;
 import java.util.function.Supplier;
 
 @Service
+@Slf4j
 public class UserService {
     @Autowired
     private UserRepository userRepository;
@@ -30,11 +33,12 @@ public class UserService {
 
     }
 
-    public UserInfoDto getUser(UserInfoDto userInfoDto){
-        Optional<UserInfo> userInfoOpt = userRepository.findByUserId(userInfoDto.getUserId());
+    public UserInfoDto getUser(String userId){
+        Optional<UserInfo> userInfoOpt = userRepository.findByUserId(userId);
         if(userInfoOpt.isEmpty()){
             throw new RuntimeException("User Not found");
         }
+        log.info("User Details {}", userInfoOpt.get());
         UserInfo userInfo= userInfoOpt.get();
         return UserInfoDto.builder()
                     .userId(userInfo.getUserId())
